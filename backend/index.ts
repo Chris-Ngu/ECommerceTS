d89ac4a6f26c719fc7809620acca15885682ac53
Christopher Nguyen <Chris_ngu@pm.me>
2020-10-07 13:49:01 -0500

    working on backend routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,3 +1,7 @@
+/*
+  WORK ON CRUDING MODELS
+*/
+
 import Express, { Request, Response } from "express";
 
 import Keyboard from "src/entities/Keyboard";
@@ -15,8 +19,36 @@ Connection.then((connection) => {
     console.log(`Server listening at http://localhost:${port}`)
   });
 
-  app.get("/", (req: Request, res: Response) => {
+  app.get("/", (_, res: Response) => {
     res.send("Hello world");
   })
+
+  // ---------      GET ALL     ---------------------------
+  app.get("/users", async (_, res: Response) => {
+    const users = await userRepository.find();
+    res.json(users);
+  });
+
+  app.get("/keyboards", async (_, res: Response) => {
+    const keyboards = await keyboardRepository.find();
+    res.json(keyboards);
+  });
+
+  // ---------      GET SPECIFIC     ---------------------------
+  app.get("/users/:id", async (req: Request, res: Response) => {
+    const foundUser = await userRepository.findOne(req.params.id);
+    return res.send(foundUser);
+  });
+
+  app.get("/keyboards/:id", async (req: Request, res: Response) => {
+    const foundKeyboard = await keyboardRepository.findOne(req.params.id);
+    return res.send(foundKeyboard);
+  });
+
+  // ---------      CREATE      ---------------------------
+
 })
+  .catch((error: unknown) => {
+    console.log(`Error occured while starting backend: ${error}`)
+  });
 
