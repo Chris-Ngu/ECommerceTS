5ad0c17529ebee9cd072ffe6fc5a5666ffe76530
Christopher Nguyen <Chris_ngu@pm.me>
2020-10-21 14:13:35 -0500

    shopping page grid areas

--- frontend/src/pages/Market.tsx
+++ frontend/src/pages/Market.tsx
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
+import '../styles/Market.scss';
+
 import Nav from '../components/Nav';
 
 const Market = () => {
     return (
         <div>
             <Nav />
-            <h1>
-                Market page
-            </h1>
+            <div className="market-container">
+
+                <div className="market-advertise">
+
+                </div>
+                <div className="market-shop-by">
+
+                </div>
+                <div className="market-featured">
+
+                </div>
+            </div>
         </div>
     )
 }
