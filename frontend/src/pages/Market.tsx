831d273c07442cf17a07810026577428584b2c20
Christopher Nguyen <Chris_ngu@pm.me>
2020-10-22 16:52:13 -0500

    Carousel images

--- frontend/src/pages/Market.tsx
+++ frontend/src/pages/Market.tsx
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
-
 import '../styles/Market.scss';
 
 import Nav from '../components/Nav';
+import Slideshow from '../components/Slideshow';
 
 const Market = () => {
+
     return (
         <div>
             <Nav />
             <div className="market-container">
 
                 <div className="market-advertise">
-
+                    <Slideshow />
                 </div>
                 <div className="market-shop-by">
 
