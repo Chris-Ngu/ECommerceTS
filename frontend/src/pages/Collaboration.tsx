87aa957af7d251ed182898ac990280893e67c8f5
Christopher Nguyen <Chris_ngu@pm.me>
2020-11-01 14:11:50 -0600

    adding placeholder page components

--- frontend/src/pages/Collaboration.tsx
+++ frontend/src/pages/Collaboration.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Button, Form, Jumbotron } from "react-bootstrap";
 import Nav from "../components/Nav";
 const Collaboration = () => {
     return (
         <div>
             <Nav />
-            <h1>Collaboration page here</h1>
+            <div>
+                <Jumbotron>
+                    <h1>Future Collaborations!</h1>
+                    <p>
+                        We are currently in the process of creating the collaboration platform
+                        Please be patient while we finalize the processes
+                    </p>
+                    <div className="collaboration-form">
+                        <Form>
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter Email Here" />
+                            <br />
+                            <Button type="submit"> Be notified</Button>
+                        </Form>
+                    </div>
+                </Jumbotron>
+            </div>
         </div>
     )
 }
