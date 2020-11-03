a1bc5fc2353a467e97de80238741a63bd902cae8
Christopher Nguyen <Chris_ngu@pm.me>
2020-11-03 16:04:59 -0600

    working on mapping updates page

--- frontend/src/components/BoardListing.tsx
+++ frontend/src/components/BoardListing.tsx
@@ -1,17 +1,28 @@
+import "../styles/components/BoardListing.scss";
+
 import React from "react";
+import { Image } from 'react-bootstrap';
 
 type ListingRequirements = {
     listingTitle: string
     photoLink: string
     price: number
-    datePosted: Date
+    datePosted: string
 }
 
 const BoardListing = (props: ListingRequirements) => {
     return (
         <div className="boardlisting-container">
-            <p>{props.title}</p>
-        </div>
+            <Image
+                src={require("../images/placeholder-board.png")}
+                alt="listing-image"
+                width={350}
+                height={150}
+            />
+            <h4>{props.listingTitle}</h4>
+            <p className="boardlisting-date">{props.datePosted}</p>
+            <p className="boardlisting-price">${props.price}</p>
+        </div >
     )
 }
 
--- frontend/src/pages/Updates.tsx
+++ frontend/src/pages/Updates.tsx
@@ -1,18 +1,53 @@
+import "../styles/Updates.scss";
 import React from "react";
 
 import Nav from "../components/Nav";
 import BoardListing from "../components/BoardListing";
 
+
+
 const Updates = () => {
+    const sampleData = [
+        {
+            listingTitle: "1 title",
+            photoLink: "1 link",
+            price: 1,
+            datePosted: "1 date"
+        }, {
+            listingTitle: "2 title",
+            photoLink: "2 link",
+            price: 2,
+            datePosted: "2 date"
+        },
+    ]
+
+    const updatesSchedules = pullUpdates();
+
     return (
         <div>
             <Nav />
-            <h1>Updates page here</h1>
-            <BoardListing 
-                title="ExampleListingTItle"
-            />
+            <div className="updates-container">
+                <h1 className="updates-header">Updates page here</h1>
+                <br />
+                {
+                    sampleData.map((listing) => {
+                        return (
+                            <BoardListing
+                                listingTitle={listing.listingTitle}
+                                photoLink={listing.photoLink}
+                                price={listing.price}
+                                datePosted={listing.datePosted}
+                            />
+                        )
+                    })
+                }
+            </div>
         </div>
     )
+}
+
+const pullUpdates = () => {
+    // pull update catalogue here
 }
 
 export default Updates;
