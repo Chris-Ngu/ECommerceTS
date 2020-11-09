ec42085721813929a6e180a0cda1fea4dd41e92e
Christopher Nguyen <Chris_ngu@pm.me>
2020-11-09 14:06:11 -0600

    initial learning page

--- frontend/src/pages/Learn.tsx
+++ frontend/src/pages/Learn.tsx
@@ -1,3 +1,5 @@
+import "../styles/Learn.scss";
+
 import React from "react";
 import Nav from "../components/Nav";
 
@@ -5,7 +7,30 @@ const Learn = () => {
     return (
         <div>
             <Nav />
-            <h1>Learn page here</h1>
+            <div>
+                <div className="learn-guide-container">
+                    <div className="learn-guide-switch">
+                        <h3>Switch guide</h3>
+                        <p>See how thousands of users rate dozens of mechanical switches and contribute your own experience</p>
+                    </div>
+                    <div className="learn-guide-poll">
+                        <h3>Keyboard Polls</h3>
+                        <p>Vote and see how other enthusiasts vote on various mechanical keyboard topics</p>
+                    </div>
+                    <div className="learn-guide-terms">
+                        <h3>Keyboard terms</h3>
+                        <p>What is Keyrollover... ping?... Key Bounce? Find definitions and explanations to these mechanical keyboard terms and many others</p>
+                    </div>
+                    <div className="learn-guide-faqs">
+                        <h3>Keyboard Faqs</h3>
+                        <p>Find answers to common questions revealing things like differences between mechanical switches, what to look for in a keyboard, and more</p>
+                    </div>
+                    <div className="learn-guide-review">
+                        <h3>Product reviews</h3>
+                        <p>See what other users have to say and how they rank myriad products on keebclack.com</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
