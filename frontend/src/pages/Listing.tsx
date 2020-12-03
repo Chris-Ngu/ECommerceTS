c7cbf025985b45b50cf400236c99af99d9946bd9
Christopher Nguyen <Chris_ngu@pm.me>
2020-12-03 16:18:19 -0600

    adding search page images

--- frontend/src/pages/Listing.tsx
+++ frontend/src/pages/Listing.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import { SERVER_ADDRESS } from "../serverConfig";
+import Nav from "../components/Nav";
+import { Container, Image, Row, Col } from "react-bootstrap";
+
 
 const Listing: React.FC = (props: any) => {
     const [keyboardId, changeKeyboardId] = useState("");
-    const [keyboardData, changeKeyboardData] = useState({});
+    const [keyboardData, changeKeyboardData] = useState<any>({});
 
     const checkIfExists = () => {
         const id = props.match.params.keyboardId;
@@ -20,21 +23,45 @@ const Listing: React.FC = (props: any) => {
             });
     }
 
+    const checkForNullPhoto = () => {
+        return (
+            keyboardData.photoLink === null || keyboardData.photoLink === undefined
+                ? <Image
+                    src={require("../images/placeholder-board.png")}
+                    width={400}
+                    height={200}
+                />
+                : <Image
+                    src={keyboardData.photoLink}
+                    width={400}
+                    height={200}
+                />
+        )
+    }
+
     useEffect(() => {
         checkIfExists();
     }, []);
 
     return (
         <div>
-            <h1>
-                {
-                    keyboardId
-                }
-                {
-                    
-                    console.log(keyboardData)
-                }
-            </h1>
+            <Nav />
+            <Container>
+                <Row>
+                    <Col>
+                        {
+                            checkForNullPhoto()
+                        }
+                    </Col>
+                    <Col>
+                        <h1>
+                            {
+                                keyboardId
+                            }
+                        </h1>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
